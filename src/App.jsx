57f4393da6d21d67b731a59e3f0adc7873dccba3
ivanherreradev <ivanh.developer@gmail.com>
2023-06-24 17:54:01 -0300

    add filters

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,28 @@ function App () {
   const [characters, setCharacters] = useState([])
   const [data, setData] = useState({})
   const [page, setPage] = useState(1)
+  const [gender, setGender] = useState('')
+  const [status, setStatus] = useState('')
+  const [species, setSpecies] = useState('')
 
-  const API = `https://rickandmortyapi.com/api/character?page=${page}`
+  const API = `https://rickandmortyapi.com/api/character/?page=${page}&gender=${gender}&status=${status}&species=${species}`
+
+  // options for select
+  const genders = ['female', 'male', 'genderless', 'unknown']
+  const characterStatus = ['alive', 'dead', 'unknown']
+  const characterSpecies = [
+    'human',
+    'alien',
+    'humanoid',
+    'poopybutthole',
+    'mythological',
+    'unknown',
+    'animal',
+    'disease',
+    'robot',
+    'cronenberg',
+    'planet'
+  ]
 
   useEffect(() => {
     fetch(API)
@@ -31,11 +51,62 @@ function App () {
     }
   }
 
+  const handleFilterGender = (value) => {
+    setGender(value)
+  }
+
+  const handleFilterStatus = (value) => {
+    setStatus(value)
+  }
+
+  const handleFilterSpecies = (value) => {
+    setSpecies(value)
+  }
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Rick & Morty Wiki</h1>
+
+      {/* Pagination */}
       <button onClick={handlePrevPage}>Prev</button>
       <button onClick={handleNextPage}>Next</button>
+
+      {/* Filters */}
+      {/* Gender */}
+      <select onChange={(e) => handleFilterGender(e.target.value)}>
+        <option value={''}>All Genders</option>
+        {genders.map((gender) => {
+          return (
+            <option key={gender} value={gender}>
+              {gender}
+            </option>
+          )
+        })}
+      </select>
+      {/* Status */}
+      <select onChange={(e) => handleFilterStatus(e.target.value)}>
+        <option value={''}>All Status</option>
+        {characterStatus.map((status) => {
+          return (
+            <option key={status} value={status}>
+              {status}
+            </option>
+          )
+        })}
+      </select>
+      {/* Species */}
+      <select onChange={(e) => handleFilterSpecies(e.target.value)}>
+        <option value={''}>All Species</option>
+        {characterSpecies.map((species) => {
+          return (
+            <option key={species} value={species}>
+              {species}
+            </option>
+          )
+        })}
+      </select>
+
+      {/* Characters */}
       <ul>
         {characters.map((character) => {
           return (
